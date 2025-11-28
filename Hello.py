0c3696e54c7c3e4c99f5c27b9f921c6ecd4b952d
Chen, Jared <Jared.Chen@suss.com>
2025-11-28 15:10:50 +0800

    feat: 新增兩個 Python 腳本，一個用於擲骰子，另一個用於打印問候語。

---
+++ Hello.py
@@ -0,0 +1,16 @@
+# This program prints a greeting message
+# It includes the name and age of a person
+
+
+# The name of the person
+name = 'Jared'
+
+# The age of the person
+age = 35
+
+"""
+This function prints a greeting message
+It includes the name and age of a person
+"""
+print(f"Hello my name is {name} and I am {age} years old")
+print("Hello my name is",name ,"and I am",age," years old")
